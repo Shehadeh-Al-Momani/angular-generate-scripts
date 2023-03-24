9bf820384c799c59c0a605c1183a419d15133018
Shehadeh-Al-Momani <70090805+Shehadeh-Al-Momani@users.noreply.github.com>
2023-03-24 18:21:03 +0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,16 +17,8 @@ import { HttpClientModule } from '@angular/common/http';
   providers: [
     {
       provide: HIGHLIGHT_OPTIONS,
-      useValue: <HighlightOptions>{
-        lineNumbers: true,
-        // coreLibraryLoader: () => import('highlight.js/lib/core'),
-        // lineNumbersLoader: () => import('highlightjs-line-numbers.js'),
-        themePath: 'node_modules/highlight.js/styles/github.css',
-        // languages: {
-        //   typescript: () => import('highlight.js/lib/languages/typescript'),
-        //   css: () => import('highlight.js/lib/languages/css'),
-        //   xml: () => import('highlight.js/lib/languages/xml'),
-        // },
+      useValue: {
+        fullLibraryLoader: () => import('highlight.js'),
       },
     },
   ],
