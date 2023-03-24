c2bc3d742182e75943c353ad8a2af65012413ae3
Shehadeh-Al-Momani <70090805+Shehadeh-Al-Momani@users.noreply.github.com>
2023-03-24 18:26:58 +0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,8 +17,17 @@ import { HttpClientModule } from '@angular/common/http';
   providers: [
     {
       provide: HIGHLIGHT_OPTIONS,
-      useValue: {
-        fullLibraryLoader: () => import('highlight.js'),
+      useValue: <HighlightOptions>{
+        lineNumbers: true,
+        coreLibraryLoader: () => import('highlight.js/lib/core'),
+        lineNumbersLoader: () => import('highlightjs-line-numbers.js'),
+        themePath: 'node_modules/highlight.js/styles/github.css',
+        languages: {
+          typescript: () => import('highlight.js/lib/languages/typescript'),
+          css: () => import('highlight.js/lib/languages/css'),
+          xml: () => import('highlight.js/lib/languages/xml'),
+        },
+        themePath: 'assets/styles/solarized-dark.css',
       },
     },
   ],
